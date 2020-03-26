9cca17813c013f089997d4931d44b9906de07390
Andreas Regner <andreas.regner@swisslife.ch>
2020-03-26 09:50:39 +0100

    For testing purposes, change document request

--- Controllers/RequestController.cs
+++ Controllers/RequestController.cs
@@ -45,12 +45,12 @@ namespace SwissLife.Slkv.Partner.ClientAppSample.Controllers
             return View(model);
         }
 
-        public async Task<IActionResult> Documents(string contractId = "B01482DD9BED4927BEB5BB54EFD6D6E2")
+        public async Task<IActionResult> Documents(string refCorrelationId = "3921DDC2-3587-4B65-91F0-D30278C8FB5A")
         {
             string accessToken = await HttpContext.GetTokenAsync("access_token");
             HttpClient httpClient = httpClientFactory.CreateClient();
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            HttpResponseMessage response = await httpClient.GetAsync($"https://{configuration["SlkvPartnerApi:Endpoint"]}/document?contractId={contractId}");
+            HttpResponseMessage response = await httpClient.GetAsync($"https://{configuration["SlkvPartnerApi:Endpoint"]}/document?refCorrelationId={refCorrelationId}");
             string json = await response.Content.ReadAsStringAsync();
             DocumentsModel model = JsonConvert.DeserializeObject<DocumentsModel>(json);
             return View(model);
